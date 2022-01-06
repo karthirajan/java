64962243d5979d3efeeb767744abbf2db03b60ab
karthirajan <kajekarthi@gmail.com>
2022-01-06 20:07:33 +0800

    Today task

--- src/org/sixth/TodayTask.java
+++ src/org/sixth/TodayTask.java
@@ -2,11 +2,37 @@ package org.sixth;
 
 public class TodayTask {
 	
-	public static void main(String[] args) {
-		
-		
-		System.out.println("Demo");
-		
+	private int id;
+	
+	private String mail;
+	
+	private float sal;
+	
+	public int getId() {
+		return id;
 	}
 
+	public void setId(int i) {
+		this.id = i;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public float getSal() {
+		return sal;
+	}
+
+	public void setSal(float sal) {
+		this.sal = sal;
+	}
+	
+	
+	
+	
 }
