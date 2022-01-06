9d927ce592c9d04f2f5a698306a224cbc773ef0d
unknown <mail>
2022-01-06 22:26:41 +0530

    Tharaproject

---
+++ src/org/tharagit/Git.java
@@ -0,0 +1,35 @@
+package org.tharagit;
+
+
+public class Git {
+	public void PhoneName() {
+		System.out.println("Note10");
+		
+	}
+public void PhoneMiNo() {
+	System.out.println("12345677");
+		
+	}
+public void Camera() {
+	System.out.println("64MP");
+	
+}
+public void Storage() {
+	System.out.println("128");
+}
+public void PhoneOs() {
+	System.out.println("Mi10");
+}
+public static void main(String[] args) {
+	Git k= new Git();
+	k.PhoneName();
+	k.PhoneMiNo();
+	k.Camera();
+	k.Storage();
+	k.PhoneOs();
+}
+}
+
+
+
+
